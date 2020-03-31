2431ff0eeb2974a5b279898b3a05dd4c09a5d7fe
cthutq25 <cthutq0632@gmail.com>
2020-03-31 15:01:21 +0300

    compas

---
+++ app/src/main/java/com/example/mycompass/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.mycompass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView ivDinamic;
+    private TextView textView;
+    private float curent_degree = 0;
+    private SensorManager sensorManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init(){
+        ivDinamic = findViewById(R.id.ivDinamic);
+        textView = findViewById(R.id.textView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),sensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float degree = Math.round(event.values[0]);
+
+        textView.setText("Degree from North: " + Float.toString(degree) + " degrees");
+
+
+        RotateAnimation ra = new RotateAnimation(curent_degree, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+        ra.setDuration(210);
+
+        ra.setFillAfter(true);
+
+        ivDinamic.startAnimation(ra);
+        curent_degree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
